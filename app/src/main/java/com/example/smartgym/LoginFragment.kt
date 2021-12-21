0f14519f47c840983d1a0bf1e6f172a090f58079
brucetrindade <brucetrindade2000@gmail.com>
2021-12-21 16:39:16 -0300

    SmartGYM Login Done

---
+++ app/src/main/java/com/example/smartgym/LoginFragment.kt
@@ -0,0 +1,82 @@
+package com.example.smartgym
+
+import android.app.Activity
+import android.content.Intent
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginFragment : Fragment() {
+
+    private lateinit var viewModel: LoginViewModel
+    private var REQUEST_CODE = 1001
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build()
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.login_fragment, container, false)
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder().setIsSmartLockEnabled(true)
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.ic_logo_smartgym) // Set logo drawable
+                .setTheme(R.style.LoginTheme) // Set theme
+                .build(),
+
+            REQUEST_CODE
+        )
+        return view
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val firebaseUser = FirebaseAuth.getInstance().currentUser
+            } else {
+                // sign in failed
+            }
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
+//
+//    private lateinit var viewModel: LoginViewModel
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return inflater.inflate(R.layout.login_fragment, container, false)
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+//        // TODO: Use the ViewModel
+//    }
+//
+//}
--- app/src/main/java/com/example/smartgym/MainActivity.kt
+++ app/src/main/java/com/example/smartgym/MainActivity.kt
@@ -1,58 +1,44 @@
 package com.example.smartgym
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import com.example.smartgym.repository.Exercises
+import android.widget.Toast
 import com.example.smartgym.repository.FirebaseRepo
 import com.example.smartgym.view.ExerciseFragment
 import com.example.smartgym.view.WorkoutFragment
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val firebaseRepo: FirebaseRepo = FirebaseRepo()
 
-    private var postList: List<Exercises> = ArrayList()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (firebaseRepo.getUser() == null){
-            firebaseRepo.createUser().addOnCompleteListener {
-                if (it.isSuccessful){
-                    buttonExercises.setOnClickListener{
-                        callExerciseFragment()
-                    }
-                    buttonWorkout.setOnClickListener{
-                        callWorkoutFragment()
-                    }
-                    buttonHome.setOnClickListener {
-                        startMain()
-                    }
-                } else {
-                    Log.d(TAG, "Error: ${it.exception!!.message}")
-                }
-            }
-        } else {
-            buttonExercises.setOnClickListener{
-                callExerciseFragment()
-            }
-            buttonWorkout.setOnClickListener{
-                callWorkoutFragment()
-            }
-        }
+        if (firebaseRepo.getUser() == null)  callLoginFragment()
 
+        setupButtonFunctions()
 
     }
 
-    private fun startMain(){
-        val i = Intent(this, MainActivity::class.java)
-        startActivity(i)
+    private fun setupButtonFunctions(){
+        buttonExercises.setOnClickListener{
+            callExerciseFragment()
+        }
+        buttonWorkout.setOnClickListener{
+            callWorkoutFragment()
+        }
+    }
+
+    private fun callLoginFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.layoutMain, LoginFragment())
+            .commit()
     }
 
     private fun callExerciseFragment(){
@@ -64,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         containerDicas.visibility = View.GONE
 
     }
+
     private fun callWorkoutFragment(){
         supportFragmentManager
             .beginTransaction()
